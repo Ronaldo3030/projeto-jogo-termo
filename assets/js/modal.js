9ed310366dc2ef4deab51287b135a48101108654
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-07-04 22:36:07 -0300

    ajustado bug nos modais

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,5 @@
+let url = window.location.href;
+
 let backgroundModal = document.getElementById('background-modal')
 let modal = document.getElementById('modal')
 let btnComoJogar = document.getElementById('btnComoJogar')
@@ -7,14 +9,17 @@ backgroundModal.addEventListener('click', (e) => {
     }
 })
 
-let backgroundModal2 = document.getElementById('background-modal2')
-let modal2 = document.getElementById('modal2')
-backgroundModal2.addEventListener('click', (e) => {
-    if (e.target.classList.value == 'background-modal' || e.target.classList.value == 'close') {
-        backgroundModal2.classList.add('d-none')
-    }
-})
+if(url.includes('play')){
+    let backgroundModal2 = document.getElementById('background-modal2')
+    let modal2 = document.getElementById('modal2')
+    backgroundModal2.addEventListener('click', (e) => {
+        if (e.target.classList.value == 'background-modal' || e.target.classList.value == 'close') {
+            backgroundModal2.classList.add('d-none')
+        }
+    })
+}else{
+    btnComoJogar.addEventListener('click', () => {
+        backgroundModal.classList.remove('d-none')
+    })
+}
 
-btnComoJogar.addEventListener('click', () => {
-    backgroundModal.classList.remove('d-none')
-})
